f3c1c0c734d5cf868ffa986ab38a33ff01e7a373
Teja Kasukurthi <tkasukurthi@innominds.com>
2021-09-17 00:09:42 +0530

    Added changes to contact us

--- src/components/ContactUs/ContactForm.tsx
+++ src/components/ContactUs/ContactForm.tsx
@@ -3,6 +3,7 @@ import {
     Typography,
     Box,
     TextField,
+    Button,
    
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
@@ -28,19 +29,31 @@ export const ContactForm = ()=>{
     return (
         <Container fixed>
             <Typography  classes={{ root : classes.root}}>Message Us</Typography>
-            <Box
+            <form>
+      <TextField  size="small"  margin="normal" label="Full Name" variant="outlined" />
+      <TextField  size="small" margin="normal"  label="Email" variant="outlined"/>
+      <TextField  size="small" margin="normal" label="Mobile" variant="outlined" />
+      <TextField  size="small" margin="normal" label="Subject" variant="outlined" />
+      <TextField  size="small" fullWidth margin="normal" label="Message" variant="outlined" />
+    </form>
+            {/* <Box
       component="form"
+      sx={{
+        width: 500,
+        maxWidth: '100%',
+      }}
     //   sx={{
     //     '& > :not(style)': { m: 1, width: '25ch' },
     //   }}
     //   noValidate
     //   autoComplete="off"
     >
-      <TextField id="outlined-basic" label="First Name" variant="outlined" />
-      <TextField id="outlined-basic" label="Last Name" variant="outlined" />
+      <TextField id="outlined-basic" label="Your Name" variant="outlined" />
+      <TextField id="outlined-basic" label="Email id" variant="outlined" />
       <TextField id="outlined-basic" label="Mobile" variant="outlined" />
-      <TextField id="outlined-basic" label="Last Name" variant="outlined" />
-    </Box>
+      <TextField id="outlined-basic" label="Subject" variant="outlined" />
+       <TextField id="outlined-basic" label="Your message" fullWidth variant="outlined" />
+    </Box> */}
         </Container>
     )
 }
